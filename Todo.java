1eadedda170587ed52beaeb32348327075ce9f19
macbook air <macbookair@macbooks-MacBook-Air.local>
2024-12-04 17:52:47 +0600

    first task

---
+++ Todo.java
@@ -0,0 +1,20 @@
+public class Todo {
+
+
+    static   int myAge = 25;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World 2 " + myAge);
+
+        final String t = "text";
+        System.out.println(t);
+
+//        t = "hi ";
+        System.out.println(t);
+
+        System.out.println(myAge);
+
+    }
+
+
+}
